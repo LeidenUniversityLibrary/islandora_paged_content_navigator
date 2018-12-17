dbddeae6f9ba99364e81bd163c24258f4868ee5e
lucasvanschaik <schaik@library.leidenuniv.nl>
2018-12-17 15:03:50 +0000

    More like islandora-solr-search-navigation-block
    Implemented the previous and next links for a book more like the
    islandora-solr-search-navigation-block: with a template file that
    can be changed thru a theme and with the same rendering array.

---
+++ theme/islandora-paged-content-navigation-block.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Islandora paged content navigation block.
+ *
+ * Variables available:
+ * - $return_link: link to return to the book.
+ * - $prev_link: Link to previous page of the book.
+ * - $next_link: link to next page of the book.
+ *
+ */
+?>
+
+
+  <div id="islandora-paged-content-return-link"><?php print $return_link; ?></div>
+  <div id="islandora-paged-content-prev-link"><?php print $prev_link; ?></div>
+  <div id="islandora-paged-content-next-link"><?php print $next_link; ?></div>
+
